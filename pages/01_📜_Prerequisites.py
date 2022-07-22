6fea9249d485700010b39bdd46e447d72e9f1f99
Harshvardhan Tiwari <hv.tiwari178@gmail.com>
2022-07-22 15:28:53 +0530

    Added intro and page config

--- pages/01_📜_Prerequisites.py
+++ pages/01_📜_Prerequisites.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from PIL import Image
+
+## Page configuration and customization
+img = Image.open("whatsapp_image2.png")
+st.set_page_config(page_title='Chat Analyzer', page_icon = img, layout = 'wide')
 
 ## Hide Footer and Hamburger Menu
 hide_streamlit_style = """
@@ -13,6 +18,7 @@ st.title("Prerequisties")
 
 st.subheader("1. The time format of your smartphone must be in 12h format. You can verify/change it in your phone's 'Date & Time' settings.")
 st.subheader("2. The language of your smartphone must be English (USA). You can find it in the 'Languages & Input' tab in your phone's 'System' settings.")
+st.markdown("Note: If you have changed any of the above settings in your smartphone, reboot your phone once before downloading the data from WhatsApp.")
 st.subheader("3. You need to have the exported chat data which you want to analyze. To export and download the chat data, just follow the steps below:")
 
 st.header(" ")
